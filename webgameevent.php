5b111121101ad446e943e8e398d6407a40ee8446
Mohd Musharraf <31364311+mdmshf@users.noreply.github.com>
2017-12-29 00:35:00 +0530

    Update webgameevent.php

--- webgameevent.php
+++ webgameevent.php
@@ -8,17 +8,8 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
-<script> 
-jQuery(document).ready(function($) {
-  $('#header').load('header.html', function () {
-    console.log('header.html loaded')
-  });
-  $('#footer').load('footer.html', function () {
-    console.log('footer.html loaded')
-  });
-})
-</script>
+    <script src="w3data.js"></script>
+
 </head>
 <body style="margin-bottom: 0px;">
 
@@ -26,7 +17,7 @@ jQuery(document).ready(function($) {
 
 
 <!--======================================NAVBAR STARTS HERE=============================================-->
-<div id="header"></div>
+<div w3-include-html="header.html"></div>
 <!--======================================NAVBAR ENDS HERE=============================================-->
 
 
